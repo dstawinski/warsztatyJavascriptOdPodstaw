d3fc8a2b08f7c6111c8e1dac18e66016b722b49c
Dominik Stawiński <Dominik.Stawinski@dreamlab.pl>
2019-03-25 17:07:43 +0100

    Zajecia 1

---
+++ Zajęcia 1/PakietStartowy/index.js
@@ -0,0 +1,12 @@
+// Zadanie 0: Napisz funkcję która wypisze na ekranie powitanie dla osoby o podanym imieniu
+
+// Zadanie 1: Napisz funkcję 2-argumentową,
+// która zwróci prawdę gdy jedna z podanych liczb będzie równa 50
+// lub ich suma będzie równa 50
+
+// Zadanie 2: Napisz funkcję która odwróci podaną jej liczbę 1234 -> 4321
+
+// Zadanie 3: Napisz funkcję która policzy objętość walca o zadanych parametrach.
+// Walec ma być obiektem, który posiada wysokość i promień podstawy
+
+// Zadanie 4: napisz pętlę która przejdzie od 0 do 15 i na każdej liczbie wypisze jej parzystość.
