796f910a02529ed56f99d7c344be517f7d5c3fc9
Dominik Stawiński <Dominik.Stawinski@dreamlab.pl>
2019-05-06 20:15:34 +0200

    Case

--- Zajęcia 4/PakietStartowy/index.js
+++ Zajęcia 4/PakietStartowy/index.js
@@ -6,27 +6,23 @@
 // 3: processing request
 // 4: request finished and response is ready
 
-let displayText;
-// Wczytanie zwykłego pliku txt
-const xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        displayText = this.responseText;
-        // alert(this.responseText)
-        const div = document.createElement('div')
-        console.log(div)
-
-        // const htmlDiv = document.querySelector('div')
-        // console.log(htmlDiv)
-        const h1 = document.createElement('h1')
-        h1.innerText = displayText;
-        div.appendChild(h1)
-        // console.log(div)
-        document.getElementById('root').appendChild(div)
-    }
-};
-xhttp.open("GET", "test1.txt", true);
-xhttp.send();
+// let displayText;
+// // Wczytanie zwykłego pliku txt
+// const xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function () {
+//     if (this.readyState === 4 && this.status === 200) {
+//         displayText = this.responseText;
+//         // alert(this.responseText)
+//         const div = document.createElement('div')
+//         const h1 = document.createElement('h1')
+//         h1.innerText = displayText;
+//         div.appendChild(h1)
+//         // console.log(div)
+//         document.getElementById('root').appendChild(div)
+//     }
+// };
+// xhttp.open("GET", "test1.txt", true);
+// xhttp.send();
 
 // // Wczytanie pliku XML
 // const xhttp = new XMLHttpRequest();
@@ -41,24 +37,24 @@ xhttp.send();
 // xhttp.open("GET", "test2.xml", true);
 // xhttp.send();
 
-// // Wczytanie pliku json
-// const xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-//         // console.log(this.responseText);
-//         const parsedFile = JSON.parse(this.responseText)
-//         // console.log(parsedFile)
-//         alert(parsedFile.texts)
-//         for (let i = 0; i < parsedFile.texts.length; i++) console.log(parsedFile.texts[i])
-//     }
-// };
-// xhttp.open("GET", "test3.json", true);
-// xhttp.send();
+// Wczytanie pliku json
+const xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        // console.log(this.responseText);
+        const parsedFile = JSON.parse(this.responseText)
+        // console.log(parsedFile)
+        alert(parsedFile.texts)
+        for (let i = 0; i < parsedFile.texts.length; i++) console.log(parsedFile.texts[i])
+    }
+};
+xhttp.open("GET", "test3.json", true);
+xhttp.send();
 
 // Tworzenie i podłączanie elementów html
 
 
-h1.addEventListener('click', () => h1.style.backgroundColor = 'blue')
+// h1.addEventListener('click', () => h1.style.backgroundColor = 'blue')
 
 // const ol = document.createElement('ol')
 
